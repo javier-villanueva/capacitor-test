905dfc862493020c55aacd5f41644dd2104e5d65
Javier Villanueva <javier.villanueva@tryrook.io>
2025-01-03 09:53:15 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'empty-cap',
+  webDir: 'dist'
+};
+
+export default config;
